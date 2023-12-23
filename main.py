1a55a2fcad5dc72ba4d44265add098d30c9b7af3
justAplaer <143881241+justAplaer@users.noreply.github.com>
2023-12-23 18:34:54 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#Импорт
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+#Запуск страницы с контентом
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+#Динамичные скиллы
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    button_html = request.form.get('button_html')
+    button_dateBase = request.form.get('button_dateBase')
+    return render_template('index.html', button_python=button_python,
+                           button_discord = button_discord,
+                           button_html = button_html,
+                           button_dateBase = button_dateBase)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
